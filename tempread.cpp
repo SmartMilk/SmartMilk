39e871f25f2ab1915e2bb4114601a336ea1d5e52
A. Jamieson <agjamieson88@gmail.com>
2018-04-10 17:49:25 +0100

    re-formatted tempread function

--- tempread.cpp
+++ tempread.cpp
@@ -3,74 +3,39 @@
 using namespace std;
 
 double defInput = 0.0;
-double tempC = 0.0;
-
 
 //temperature reading function
 void Tempread::run() //temperature reading function adapted from: http://bradsrpi.blogspot.co.uk/2013/12/c-program-to-read-temperature-from-1.html
 {
-
-	DIR *dir;
-	struct dirent *dirent;
-	char dev[16];      // Dev ID
-	char devPath[128]; // Path to device
-	char buf[256];     // Data from device
-	char tmpData[6];   // Temp C * 1000 reported by device
-	char path[] = "/sys/bus/w1/devices";
-	ssize_t numRead;
-	dir = opendir(path);
+	char path[] = "/sys/bus/w1/devices/28-0317607d35ff/w1_slave";
+	char buf[256];
+	int fd = -1;
+	char *temp;
+	double value;
 
 	runningX = true;
 	while (runningX)
 	{
 
 
-		if (dir != NULL)
+		fd = open(path, O_RDONLY);
+		if (fd == -1)
 		{
-			while ((dirent = readdir(dir)))
-				// 1-wire devices are links beginning with 28-
-				if (dirent->d_type == DT_LNK &&
-					strstr(dirent->d_name, "28-0317607d35ff") != NULL) {
-					strcpy(dev, dirent->d_name);
-					printf("\nDevice: %s\n", dev);
-				}
-			(void)closedir(dir);
+			perror("Couldn't open the w1 device.");
+			exit(1);
 		}
-		else
-		{
-			perror("Couldn't open the w1 devices directory");
-		//	return 1;
-		}
+		read(fd, buf, sizeof(buf));
+		temp = strchr(buf, 't');
 
-		// Assemble path to OneWire device
-		sprintf(devPath, "%s/%s/w1_slave", path, dev);
-		// Read temp continuously
-		// Opening the device's file triggers new reading
-	//	while (1) {
-			int fd = open(devPath, O_RDONLY);
-			if (fd == -1)
-			{
-				perror("Couldn't open the w1 device.");
-		//		return NULL;
-			}
-			while ((numRead = read(fd, buf, 256)) > 0)
-			{
-				strncpy(tmpData, strstr(buf, "t=") + 2, 5);
-				double tempC = strtof(tmpData, NULL);
-				 printf("Device: %s  - ", dev);
-				 printf("Temp: %.3f C  ", tempC / 1000);
-				 printf("%.3f F\n\n", (tempC / 1000) * 9 / 5 + 32);
-			//new format:
-				defInput = tempC / 1000; //defining temperature reading input
+		sscanf(temp, "t=%s", temp);
 
-				//old format:
-				//	*a = tempC / 1000;
-				//	double result = *a;
-				//	return result;
-			}
-			close(fd);
-		//}
-		//usleep(500000);
+		value = atof(temp) / 1000;
+
+		printf("Device: %s  - ", dev);
+		printf("Temp: %.3f C  ", value / 1000);
+		printf("%.3f F\n\n", (value / 1000) * 9 / 5 + 32);
+
+		double defInput = value; 
 	}
 }
 
