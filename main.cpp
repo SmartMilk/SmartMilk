f7ccca6effe51b1ed41d736f384c23fe78c06f49
A. Jamieson <agjamieson88@gmail.com>
2018-04-11 13:03:57 +0100

    comments

--- main.cpp
+++ main.cpp
@@ -6,28 +6,20 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+	
 	//create splash screen
 	QPixmap pixmap ("./pics/logo.jpg");
 	QSplashScreen splash (pixmap);
 	splash.show();
 	app.processEvents();
 	usleep(3000000);
-    // create the window
-    Window window;
-    window.showMaximized();
+	
+    	// create the window
+    	Window window;
+ 	window.showMaximized();
+	
 	//close splash screen
 	splash.finish(&window);
-
-//		QThread thread;
-//		QObject::connect(&thread, SIGNAL(started()), &window, SLOT(plotUpdate()));
-//		QObject::connect(&thread, SIGNAL(finished()), &app, SLOT(quit()));
-
-
-//		window.moveToThread(&thread);
-//		thread.start();
-
-        // call the window.timerEvent function every _ms
-//    window.startTimer(500);
-        // execute the application
+	
     return app.exec();
 }
